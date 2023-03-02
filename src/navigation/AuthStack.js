7447c974d2d56ebd326e3534d1b8137e90ac8894
nkushwaha1993 <kush.neeraj1993@gmail.com>
2023-03-02 21:42:55 +0530

    login api integration

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
 import LoginScreen from "../screens/LoginScreen";
 
 const Stack = createNativeStackNavigator();
