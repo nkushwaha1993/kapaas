50a3a7aa7a23579bc0eff3ca2a52f084020ef5b3
pratik-malviya <pratt.malviya@gmail.com>
2023-03-28 20:32:25 +0530

    Added vendor search page and service.

--- src/screens/VendorSearchScreen.js
+++ src/screens/VendorSearchScreen.js
@@ -1,163 +1,149 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+} from "react-native";
+import { vendorSearch as vendorSearchService } from "../services/vendor.service";
+
+const PAGE_SIZE = 10;
 
 const VendorSearchScreen = () => {
-  const [searchText, setSearchText] = useState('');
-  const [allVendors, setAllVendors] = useState([
-    { id: 1, name: 'Vendor 1', address: 'Address 1', phone: '111-111-1111' },
-    { id: 2, name: 'Vendor 2', address: 'Address 2', phone: '222-222-2222' },
-    { id: 3, name: 'Vendor 3', address: 'Address 3', phone: '333-333-3333' },
-    { id: 4, name: 'Vendor 4', address: 'Address 4', phone: '444-444-4444' },
-    { id: 5, name: 'Vendor 5', address: 'Address 5', phone: '555-555-5555' },
-    { id: 6, name: 'Vendor 6', address: 'Address 6', phone: '666-666-6666' },
-    { id: 7, name: 'Vendor 7', address: 'Address 7', phone: '777-777-7777' },
-    { id: 8, name: 'Vendor 8', address: 'Address 8', phone: '888-888-8888' },
-    { id: 9, name: 'Vendor 9', address: 'Address 9', phone: '999-999-9999' },
-    { id: 10, name: 'Vendor 10', address: 'Address 10', phone: '000-000-0000' },
-  ]);
-  const [vendors, setVendors] = useState(allVendors);
-
+  const [searchText, setSearchText] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
   const [page, setPage] = useState(1);
-  const [pageSize] = useState(5);
 
-  const handleSearch = () => {
-    // Filter vendors based on the searchText
-    const filteredVendors = vendors.filter((vendor) =>
-      vendor.name.toLowerCase().includes(searchText.toLowerCase())
-    );
-    // Update vendors state with filtered vendors
-    setVendors(filteredVendors);
-    // Reset page to 1
-    setPage(1);
+  useEffect(() => {
+    //handleSearch();
+  }, [page]);
+
+  const handleSearch = async () => {
+    await vendorSearchService(searchText)
+      .then((response) => {
+        setSearchResults(response);
+      })
+      .catch((error) => {
+        console.log("Exception");
+      });
   };
 
-  const handlePageChange = (newPage) => {
-    if (newPage > 0 && newPage <= Math.ceil(vendors.length / pageSize)) {
-      setPage(newPage);
-    } 
+  const handleRowPress = (item) => {
+    console.log("Row pressed: ", item);
   };
 
-  const renderItem = ({ item }) => (
-    <View style={styles.row}>
-      <Text style={styles.column}>{item.name}</Text>
-      <Text style={styles.column}>{item.address}</Text>
-      <Text style={styles.column}>{item.phone}</Text>
-    </View>
+  const renderRow = ({ item }) => (
+    <TouchableOpacity
+      onPress={() => handleRowPress(item)}
+      style={styles.rowContainer}
+    >
+      <View style={styles.row}>
+        <Text style={styles.rowTitle}>{item.name}</Text>
+        <Text style={styles.rowDescription}>{item.description}</Text>
+      </View>
+    </TouchableOpacity>
   );
 
-  const renderSeparator = () => {
-    return <View style={styles.separator} />;
-  };
-
+  const render = ({ item }) => ( 
+      <TouchableOpacity>
+        <Text>
+          {item.firstName} , {item.lastName}
+        </Text>
+      </TouchableOpacity>   
+  );
   return (
     <View style={styles.container}>
-      <View style={styles.searchBoxContainer}>
+      <View style={styles.searchContainer}>
         <TextInput
-          placeholder="Search Vendors"
+          style={styles.searchInput}
+          placeholder="Search"
           value={searchText}
           onChangeText={setSearchText}
-          style={styles.searchBox}
-          onSubmitEditing={handleSearch}
         />
-      </View>
-      <FlatList
-        data={vendors.slice((page - 1) * pageSize, page * pageSize)}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id.toString()}
-        ItemSeparatorComponent={renderSeparator}
-      />
-      <View style={styles.pagination}>
-        <TouchableOpacity
-          onPress={() => handlePageChange(page - 1)}
-          disabled={page === 1}
-          style={page === 1 ? styles.disabledButton : styles.button}
-        >
-        <Text style={styles.buttonText}>Prev</Text>
+        <TouchableOpacity style={styles.searchButton} onPress={handleSearch}>
+          <Text style={styles.searchButtonText}>Search</Text>
         </TouchableOpacity>
-        <Text style={styles.pageText}>Page {page}</Text>
-        <TouchableOpacity
-          onPress={() => handlePageChange(page + 1)}
-          disabled={page * pageSize >= vendors.length}
-          style={page * pageSize >= vendors.length ? styles.disabledButton : styles.button}
-        >
-        </TouchableOpacity>   
-      </View>  
-    </View> 
+      </View>
+      
+        {searchResults && (
+          <FlatList
+            data={searchResults}
+            renderItem={render}
+            keyExtractor={(item) => item.id.toString()}
+            style={styles.resultsList}
+          />
+        )}
+      
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5FCFF',
-    padding: 10,
+    backgroundColor: "#fff",
+    paddingVertical: 24,
+    paddingHorizontal: 16,
   },
-  searchBoxContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 10,
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "top",
+    backgroundColor: "#F2F2F2",
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    marginBottom: 16,
   },
-  searchBox: {
+  searchInput: {
     flex: 1,
-    marginRight: 10,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    paddingVertical: 5,
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#333",
+    paddingVertical: 16,
+    paddingHorizontal: 8,
   },
   searchButton: {
-    backgroundColor: '#008CBA',
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 5,
+    backgroundColor: "#007AFF",
+    borderRadius: 8,
+    paddingVertical: 16,
+    paddingHorizontal: 24,
+    marginLeft: 16,
   },
   searchButtonText: {
-    color: '#fff',
-    fontWeight: 'bold',
+    color: "#fff",
+    fontWeight: "bold",
     fontSize: 16,
   },
-  tableContainer: {
+  resultsList: {
     flex: 1,
-    marginTop: 10,
-  },
-  tableHeader: {
-    flexDirection: 'row',
-    backgroundColor: '#eee',
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-  },
-  headerText: {
-    fontWeight: 'bold',
-    fontSize: 16,
-    flex: 1,
+    marginTop: 16,
   },
   rowContainer: {
-    flexDirection: 'row',
-    paddingHorizontal: 10,
-    paddingVertical: 5,
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    marginBottom: 16,
+    elevation: 2,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
   },
-  rowText: {
-    flex: 1,
+  row: {
+    padding: 16,
+  },
+  rowTitle: {
+    fontWeight: "bold",
+    fontSize: 18,
+    marginBottom: 8,
+    color: "#333",
+  },
+  rowDescription: {
     fontSize: 16,
-  },
-  paginationContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginTop: 10,
-  },
-  paginationButton: {
-    backgroundColor: '#008CBA',
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    borderRadius: 5,
-    marginHorizontal: 5,
-  },
-  paginationButtonText: {
-    color: '#fff',
-    fontWeight: 'bold',
-    fontSize: 16,
+    color: "#666",
   },
 });
 
